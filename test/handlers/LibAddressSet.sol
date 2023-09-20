1fedfe48841ef294feeb2d26ecec6f8fbaa1a10c
c-note <dongustavo20@gmail.com>
2023-09-20 13:37:04 +0100

    Add LibAddressSet library

---
+++ test/handlers/LibAddressSet.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+struct AddressSet {
+    address[] addrs;
+    mapping(address => bool) saved;
+}
+
+library LibAddressSet {
+    function add(AddressSet storage s, address addr) internal {
+        if (!s.saved[addr]) {
+            s.addrs.push(addr);
+            s.saved[addr] = true;
+        }
+    }
+
+    function contains(
+        AddressSet storage s,
+        address addr
+    ) internal view returns (bool) {
+        return s.saved[addr];
+    }
+
+    function count(AddressSet storage s) internal view returns (uint256) {
+        return s.addrs.length;
+    }
+}
