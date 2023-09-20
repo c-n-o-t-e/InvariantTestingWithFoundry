4d528c02d4e766a6073c0aedb3c8491e55b18ae8
c-note <dongustavo20@gmail.com>
2023-09-20 13:09:43 +0100

    Add ghost variables.

--- test/handlers/Handler.sol
+++ test/handlers/Handler.sol
@@ -8,6 +8,10 @@ import {StdUtils} from "forge-std/StdUtils.sol";
 
 contract Handler is CommonBase, StdCheats, StdUtils {
     WETH9 public weth;
+
+    uint256 public ghost_depositSum;
+    uint256 public ghost_withdrawSum;
+
     uint256 public constant ETH_SUPPLY = 120_500_000 ether;
 
     constructor(WETH9 _weth) {
@@ -18,17 +22,20 @@ contract Handler is CommonBase, StdCheats, StdUtils {
     function deposit(uint256 amount) public {
         amount = bound(amount, 0, address(this).balance);
         weth.deposit{value: amount}();
+        ghost_depositSum += amount;
     }
 
     function withdraw(uint256 amount) public {
         amount = bound(amount, 0, weth.balanceOf(address(this)));
         weth.withdraw(amount);
+        ghost_withdrawSum += amount;
     }
 
     function sendFallback(uint256 amount) public {
         amount = bound(amount, 0, address(this).balance);
         (bool success, ) = address(weth).call{value: amount}("");
         require(success, "sendFallback failed");
+        ghost_depositSum += amount;
     }
 
     receive() external payable {}
