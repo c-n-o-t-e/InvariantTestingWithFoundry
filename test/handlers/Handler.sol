9f3f2323c7e5754b9940fcf7bd4bb38d8d2c0bd0
c-note <dongustavo20@gmail.com>
2023-09-23 11:02:10 +0100

    Add more ghost variables and modifiers.

--- test/handlers/Handler.sol
+++ test/handlers/Handler.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.13;
 
 import {WETH9} from "../../src/WETH9.sol";
 import {CommonBase} from "forge-std/Base.sol";
-import {StdCheats} from "forge-std/StdCheats.sol";
 import {StdUtils} from "forge-std/StdUtils.sol";
-import {AddressSet, LibAddressSet} from "../helpers/LibAddressSet.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {console2 as console} from "forge-std/Test.sol";
+import {AddressSet, AddressPair, LibAddressSet} from "../helpers/LibAddressSet.sol";
 
 contract Handler is CommonBase, StdCheats, StdUtils {
     using LibAddressSet for AddressSet;
@@ -16,9 +17,22 @@ contract Handler is CommonBase, StdCheats, StdUtils {
     uint256 public ghost_depositSum;
     uint256 public ghost_withdrawSum;
 
+    uint256 public ghost_approvedSum;
+    uint256 public ghost_zeroDeposits;
+
+    uint256 public ghost_zeroApprovals;
+    uint256 public ghost_zeroTransfers;
+
+    uint256 public ghost_UsedApprovedSum;
+    uint256 public ghost_zeroWithdrawals;
+
+    uint256 public ghost_zeroTransferFroms;
+
     address currentActor;
 
     uint256 public constant ETH_SUPPLY = 120_500_000 ether;
+
+    mapping(bytes32 => uint256) public calls;
 
     constructor(WETH9 _weth) {
         weth = _weth;
@@ -31,45 +45,14 @@ contract Handler is CommonBase, StdCheats, StdUtils {
         _;
     }
 
-    function deposit(uint256 amount) public createActor {
-        amount = bound(amount, 0, address(this).balance);
-        _pay(currentActor, amount);
-
-        vm.prank(currentActor);
-        weth.deposit{value: amount}();
-
-        ghost_depositSum += amount;
+    modifier useActor(uint256 actorIndexSeed) {
+        currentActor = _actors.rand(actorIndexSeed);
+        _;
     }
 
-    function withdraw(uint256 amount) public createActor {
-        amount = bound(amount, 0, weth.balanceOf(currentActor));
-        vm.startPrank(currentActor);
-
-        weth.withdraw(amount);
-        _pay(address(this), amount);
-
-        vm.stopPrank();
-        ghost_withdrawSum += amount;
-    }
-
-    function sendFallback(uint256 amount) public createActor {
-        amount = bound(amount, 0, address(this).balance);
-        _pay(currentActor, amount);
-
-        vm.prank(currentActor);
-        (bool success, ) = address(weth).call{value: amount}("");
-
-        require(success, "sendFallback failed");
-        ghost_depositSum += amount;
-    }
-
-    function actors() external view returns (address[] memory) {
-        return _actors.addrs;
-    }
-
-    function _pay(address to, uint256 amount) internal {
-        (bool s, ) = to.call{value: amount}("");
-        require(s, "pay() failed");
+    modifier countCall(bytes32 key) {
+        calls[key]++;
+        _;
     }
 
     receive() external payable {}
