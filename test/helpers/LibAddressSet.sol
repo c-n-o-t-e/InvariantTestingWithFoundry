1abe8305faf5d979ce383c3b748790abe088f7b7
c-note <dongustavo20@gmail.com>
2023-09-23 11:08:03 +0100

    Add add() and rand().

--- test/helpers/LibAddressSet.sol
+++ test/helpers/LibAddressSet.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.13;
 
+struct AddressPair {
+    address addr1;
+    address addr2;
+}
+
 struct AddressSet {
     address[] addrs;
+    AddressPair[] addressPairs;
     mapping(address => bool) saved;
+    mapping(address => mapping(address => bool)) savedAllowance;
 }
 
 library LibAddressSet {
@@ -11,6 +18,25 @@ library LibAddressSet {
         if (!s.saved[addr]) {
             s.addrs.push(addr);
             s.saved[addr] = true;
+        }
+    }
+
+    function add(AddressSet storage s, address addr, address addr0) internal {
+        if (!s.savedAllowance[addr][addr0]) {
+            AddressPair memory newPair = AddressPair(addr, addr0);
+            s.addressPairs.push(newPair);
+            s.savedAllowance[addr][addr0] = true;
+        }
+    }
+
+    function rand(
+        AddressSet storage s,
+        uint256 seed
+    ) internal view returns (address) {
+        if (s.addrs.length > 0) {
+            return s.addrs[seed % s.addrs.length];
+        } else {
+            return address(0);
         }
     }
 
